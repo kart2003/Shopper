b0b38521fd34ff505ca2cb010f7820335a3ce8f7
Kartikey Rajput <120725252+kart2003@users.noreply.github.com>
2024-09-21 22:52:31 +0530

    Update NewCollections.jsx

--- FrontEnd/src/Components/NewCollections/NewCollections.jsx
+++ FrontEnd/src/Components/NewCollections/NewCollections.jsx
@@ -25,40 +25,5 @@ const NewCollections = () => {
   )
 }
 
-export default NewCollectionsnewcollections')
-    .then((response)=>response.json())
-    .then((data)=>setNew_collection(data))
-   },[])
-
-  return (
-    <div className='new-collections'>
-        <h1>NEW COLLECTIONS</h1>
-        <hr />
-        <div className="collections">
-            {new_collection.map((item,i)=>{
-                return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/> 
-            })}
-        </div>
-    </div>
-  )
-}
-
-export default NewCollectionsnewcollections')
-    .then((response)=>response.json())
-    .then((data)=>setNew_collection(data))
-   },[])
-
-  return (
-    <div className='new-collections'>
-        <h1>NEW COLLECTIONS</h1>
-        <hr />
-        <div className="collections">
-            {new_collection.map((item,i)=>{
-                return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/> 
-            })}
-        </div>
-    </div>
-  )
-}
-
 export default NewCollections
+
