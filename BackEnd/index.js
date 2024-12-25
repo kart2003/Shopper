94bd834368a4c7a08fda5e58eac0c934163ffe9d
Kartikey Rajput <120725252+kart2003@users.noreply.github.com>
2024-12-25 12:02:54 +0530

    Update index.js

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -10,7 +10,7 @@ const cors = require('cors');
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-app.use(cors());
+app.use(cors({ origin: 'https://shopperfe.onrender.com/' }));
 
 //db connection mongodb
 mongoose.connect('mongodb+srv://kartikey:Kart%402003@cluster0.0wupe.mongodb.net/Ecommerce')
