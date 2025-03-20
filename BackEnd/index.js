c190a965a78e1e2e32943a9e0d8054af243b34a3
Kartikey Rajput <120725252+kart2003@users.noreply.github.com>
2025-03-20 12:43:17 +0530

    Update index.js

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -15,10 +15,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     allowedHeaders: ['sessionId', 'Content-Type'],
-    exposedHeaders: ['sessionId'],
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
   })
 );
 
